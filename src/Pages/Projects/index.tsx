42d4296db0c029f2667488ee55e33311addd18b9
BrunoSantosPK <bruno.ajsch@yahoo.com>
2022-06-18 11:04:44 -0300

    adicionado gerenciamento de imagens para projetos

--- src/Pages/Projects/index.tsx
+++ src/Pages/Projects/index.tsx
@@ -1,16 +1,47 @@
 import React from "react";
 import Header from "../../Layouts/Header";
+import Footer from "../../Layouts/Footer";
+
+import "./style.css";
 import "../../global.css";
+
+import prioreScreen1 from "../../Assets/PrioreWeb/Screen-01.png";
+import archerScreen1 from "../../Assets/Archer/Tela-01.jpg";
 
 export default function Projects() {
     return (
         <div className="site">
-            <Header selectPage="Projects" />
-            <div className="header"></div>
-            <div className="content">
-                <h1>Página Projetos</h1>
+            <Header selectPage="Home" />
+
+            <div className="container-projects">
+                <div className="area-project">
+                    <div className="area-project-figures">
+                        <img src={prioreScreen1} alt="Tela de abertura do priore" />
+                    </div>
+
+                    <div className="area-project-text">
+                        <p>A versão web do Priore não apenas implementa funcionalidades da aplicação desktop, como amplia o público e melhora o rol de ferramentas. Desde a sua epigênese em 2020, muitas mudanças comportamentais em relação aos webapps ocorreram, mas o conteúdo desse projeto ainda continua. Como recurso educacional de código livre para ensino superior e médio, Priore democratiza o acesso aos artefatos de cálculo mais robustos, até então disponíveis apenas em softwares de simulações com alto valor de licenciamento.</p>
+                        <p>Aplicação disponível <a href="https://priore.netlify.app" target="_blank">aqui</a>.</p>
+                        <p>Repositório <a href="https://github.com/BrunoSantosPK/PrioreSite" target="_blank">aqui</a>.</p>
+                    </div>
+                </div>
+
+                <div className="area-project">
+                    
+
+                    <div className="area-project-text">
+                        <p>A versão web do Priore não apenas implementa funcionalidades da aplicação desktop, como amplia o público e melhora o rol de ferramentas. Desde a sua epigênese em 2020, muitas mudanças comportamentais em relação aos webapps ocorreram, mas o conteúdo desse projeto ainda continua. Como recurso educacional de código livre para ensino superior e médio, Priore democratiza o acesso aos artefatos de cálculo mais robustos, até então disponíveis apenas em softwares de simulações com alto valor de licenciamento.</p>
+                        <p>Aplicação disponível <a href="https://priore.netlify.app" target="_blank">aqui</a>.</p>
+                        <p>Repositório <a href="https://github.com/BrunoSantosPK/PrioreSite" target="_blank">aqui</a>.</p>
+                    </div>
+
+                    <div className="area-project-figures">
+                        <img src={archerScreen1} alt="Tela de abertura do priore" />
+                    </div>
+                </div>
             </div>
-            <div className="footer"></div>
+
+            <Footer />
         </div>
     );
 }
